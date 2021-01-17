dd17e22ab19833e061a91d9b686329ca7bfa8a53
Zhiheng Jiao <zhihengj@andrew.cmu.edu>
2021-01-17 15:13:57 -0500

    debugging

--- src/components/login.js
+++ src/components/login.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const redirectUri = "http://surveyapp-env.eba-r92ervxm.us-east-1.elasticbeanstalk.com/";
+const redirectUri = "http://surveyapp-env.eba-r92ervxm.us-east-1.elasticbeanstalk.com/#/";
 const APP_SECRET_BASE64 = "TERRcXJndjJtbUZNUVE6VGdtTmRON1FDb1Y2MURnRFV3ZmdvanF3eVAyY1lR";
 
 class Login extends Component {
