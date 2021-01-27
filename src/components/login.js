7874da76fa0bffccb21d3b1afc7971cff8376874
Zhiheng Jiao <zhihengj@andrew.cmu.edu>
2021-01-27 15:01:33 -0500

    move get token logic to questionnaire, and change redirect uri to questionnaire

--- src/components/login.js
+++ src/components/login.js
@@ -28,7 +28,7 @@ class Login extends Component {
         super(props);
         this.startSurvey = this.startSurvey.bind(this);
         this.redditAuthentication = this.redditAuthentication.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
+        // this.componentDidMount = this.componentDidMount.bind(this);
 
         //temp for demo
         // console.log('In(constructor), user code and state');
